3b8d08e067a9fbde22cbe435b22076003083e2d6
Elena Iaconis <eiaconis@seas.upenn.edu>
2018-02-13 16:25:11 -0500

    Blank Project Created

---
+++ Roommate App/Roommate App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roommate App
+//
+//  Created by Elena Iaconis on 2/9/18.
+//  Copyright © 2018 Team 20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
