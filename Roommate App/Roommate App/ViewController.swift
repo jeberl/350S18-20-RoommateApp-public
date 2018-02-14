f7f4d659070819e70a5e6cc3e2de4084338beb80
Elena Iaconis <eiaconis@seas.upenn.edu>
2018-02-13 19:43:02 -0500

    Homepage

--- Roommate App/Roommate App/ViewController.swift
+++ Roommate App/Roommate App/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
