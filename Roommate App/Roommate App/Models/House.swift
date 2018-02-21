aa2aee53b32fff2ddabf37197de21763739a0fee
Jesse Berliner-Sachs <jessebs@me.com>
2018-02-20 19:34:57 -0500

    Implement Database Functions

--- Roommate App/Roommate App/Models/House.swift
+++ Roommate App/Roommate App/Models/House.swift
@@ -12,15 +12,15 @@ import FirebaseDatabase
 
 struct House {
     
-    let house_id: String
+    let houseID: String
     let house_name: String
     let house_users: [String]
-    let owner: String // unique userID
+    let owner: String // unique user email
     let recent_charges: [String] // unique chargeIDs
     let recent_interactions: [String] // array of all uids for Notifs
     
-    init(house_id: String, house_name: String, house_users:[String], owner: String, recent_charges: [String], recent_interactions:[String]) {
-        self.house_id = house_id
+    init(uid: String, house_name: String, house_users:[String], owner: String, recent_charges: [String], recent_interactions:[String]) {
+        self.houseID = uid
         self.house_name = house_name
         self.house_users = house_users
         self.owner = owner
