b265635c4ab6f6b6c4bae618e747f4ea275daf31
Ajani Motta <ajanimotta@levy-ve501-0661.apn.wlan.upenn.edu>
2018-03-19 20:29:33 -0400

    Create chores layout

---
+++ Roommate App/Roommate App/Controllers/ChoreListViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ChoreListViewController.swift
+//  Roommate App
+//
+//  Created by Ajani Motta on 3/17/18.
+//  Copyright © 2018 Team 20. All rights reserved.
+//
+
+import UIKit
+
+class ChoreListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    @IBOutlet weak var choreCountLabel: UILabel!
+    @IBOutlet weak var createChoreButton: UIButton!
+    @IBOutlet weak var choreTableView: UITableView!
+    @IBOutlet weak var showCompletedButton: UIButton!
+    var currentUser : UserAccount! // Current user
+    var chores : [Chore]! = [Chore]() // Chores in the house
+    var chore_ids : [String]! = [String]() // ChoreIDs of chores in the house
+    var database : DatabaseAccess = DatabaseAccess.getInstance()
+    var currentHouse : House!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        choreTableView.delegate = self
+        choreTableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chores.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = chores[indexPath.row].title
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Roommate App/Roommate App/Controllers/CreateChoreViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CreateChoreViewController.swift
+//  Roommate App
+//
+//  Created by Ajani Motta on 3/17/18.
+//  Copyright © 2018 Team 20. All rights reserved.
+//
+
+import UIKit
+
+class CreateChoreViewController: UIViewController {
+    
+    @IBOutlet weak var choreTitleTextField: UITextField!
+    @IBOutlet weak var choreDescriptionTextField: UITextField!
+    @IBOutlet weak var userResponsibleTextField: UITextField!
+    @IBOutlet weak var createChoreButton: UIButton!
+    var currentUser : UserAccount?
+    var currentHouse : House!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func createChoreButtonPressed(_ sender: Any) {
+        let choreTitle = choreTitleTextField!.text
+        let choreDescription = choreDescriptionTextField!.text
+        let userResponsible = userResponsibleTextField!.text
+        let date = NSDate()
+        
+       
+        // Create new house object to add to database
+        var newChore = Chore(chore_title: choreTitle!, assignor: (currentUser?.nickname)! , assignee: userResponsible!, time_assigned: date, house: currentHouse.houseID!, description: choreDescription!)
+        //self.newChore = self.database!.createChore(chore: newChore)
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
