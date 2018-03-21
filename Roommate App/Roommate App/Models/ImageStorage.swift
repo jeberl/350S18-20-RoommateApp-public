e93f24c3af8086406174096b0788dbb2e146b98f
Jesse Berliner-Sachs <jessebs@me.com>
2018-03-21 15:50:32 -0400

    Pull from master

--- Roommate App/Roommate App/Models/ImageStorage.swift
+++ Roommate App/Roommate App/Models/ImageStorage.swift
@@ -14,7 +14,7 @@ import FirebaseStorage
 class ImageStorage  {
 
     static var instance : ImageStorage? = nil
-    var storage : Storage!
+    var storage : Storage?
     
     private init(){
         //Ensure app has been configured
@@ -31,7 +31,55 @@ class ImageStorage  {
         return instance!
     }
     
-    func uploadChoreImage(image : UIImage) -> String? {
+    private func database_error(_ error: Error? = nil, error_header: String = "Error", view: UIViewController) {
+        let alert = UIAlertController(title: error_header,
+                                      message: error?.localizedDescription ,
+                                      preferredStyle: .alert)
+        present_popup(alert: alert, view: view, return_to_login: false)
+    }
+    
+    
+    private func present_popup(alert: UIAlertController, view: UIViewController, return_to_login: Bool) {
+        
+        let returnAction = UIAlertAction(title:"Login Again",
+                                         style: .default,
+                                         handler:  { action in view.performSegue(withIdentifier: "loginErrorSegue", sender: self) })
+        
+        let continueAction = UIAlertAction(title: "Continue",
+                                           style: .default)
+        
+        alert.addAction(return_to_login ? returnAction : continueAction)
+        view.present(alert, animated: true, completion: nil)
+    }
+    
+    //returns false if error uploading
+    func uploadChoreImage(image : UIImage, chore_id: String, view: UIViewController){
+    
+    }
+    
+    //returns image id of image
+    func getChoreImage(imageDownloadURL : String, view: UIViewController) -> UIImage? {
         return nil
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?, choreID: String, view: UIViewController) {
+        //userPhoto.image = image
+        picker.dismiss(animated: true, completion: nil)
+        let data = UIImageJPEGRepresentation(image, 0.8)!
+        // set upload path
+        let filePath = "chore_images/\(choreID)"
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        self.storage?.reference().child(filePath).putData(data, metadata: metaData){ (metaData, error) in
+            if let error = error {
+                self.database_error(error, error_header: "Error uploading file to Database", view: view)
+                print(error.localizedDescription)
+            } else {
+                //store downloadURL
+                let downloadURL = metaData!.downloadURL()!.absoluteString
+                //store downloadURL at database
+                DatabaseAccess.getInstance().ref.child("chores/\(choreID)/imageDownloadURL").setValue(downloadURL)
+            }
+        }
     }
 }
