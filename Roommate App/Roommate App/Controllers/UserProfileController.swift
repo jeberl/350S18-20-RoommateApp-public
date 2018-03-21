6a70d6196de37d5a437aa8f32dd3316f4b1b6c6d
Nick Buckenham <Nick Buckenham>
2018-03-21 11:35:27 -0400

    more changes

--- Roommate App/Roommate App/Controllers/UserProfileController.swift
+++ Roommate App/Roommate App/Controllers/UserProfileController.swift
@@ -19,37 +19,42 @@ class UserProfileController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // closure for getUserGlobalNickname
         let userGlobalNicknameClosure = { (returnedGlobalNickname: String?) -> Void in
             self.currentGlobalNickname = returnedGlobalNickname
-            print("TEST")
-            print("\(returnedGlobalNickname!)")
-            print("\(self.currentGlobalNickname!)")
+            let textForGlobalNickname: String = "Current Global Nickname: \(self.currentGlobalNickname ?? "Error: nil Nickname")"
+            self.userProfileGlobalNickname.text = textForGlobalNickname
         }
         
+        // calls getUserGlobalNickname, handles errors
         let errorGlobalNickname = self.database.getUserGlobalNickname(for_uid: Auth.auth().currentUser?.uid, callback: userGlobalNicknameClosure)
         if errorGlobalNickname.returned_error {
             errorGlobalNickname.raiseErrorAlert(with_title: "error", view: self)
         }
         
+        // closure for getUserLocalNickname
         let userLocalNicknameClosure = { (returnedLocalNickname: String?) -> Void in
             self.currentLocalNickname = returnedLocalNickname
+            let textForLocalNickname: String = "Current Local Nickname: \(self.currentLocalNickname ?? "Error: nil Nickname")"
+            self.userProfileLocalNickname.text = textForLocalNickname
         }
         
+        // calls getUserLocalNickname, handles errors
         let errorLocalNickname = self.database.getUserLocalNickname(from_houseID: currentHouseID, callback: userLocalNicknameClosure)
         if errorLocalNickname.returned_error {
             errorLocalNickname.raiseErrorAlert(with_title: "error", view: self)
         }
         
-        print("TEST")
-        
         // displays current profile email address
-        let profileForText: String = "User Profile For: \(Auth.auth().currentUser!.email ?? "Error: nil user") \nCurrent Global Nickname:  \nCurrent Local Nickname:)"
+        let profileForText: String = "User Profile For: \(Auth.auth().currentUser!.email ?? "Error: nil User")"
         userProfileFor.text = profileForText
         
     }
     
-    // outlet for displaying user profile
+    // outlets for displaying user profile information
     @IBOutlet weak var userProfileFor: UILabel!
+    @IBOutlet weak var userProfileGlobalNickname: UILabel!
+    @IBOutlet weak var userProfileLocalNickname: UILabel!
     
     // outlets for input text fields
     @IBOutlet weak var changePasswordField: UITextField!
@@ -57,17 +62,20 @@ class UserProfileController: UIViewController {
     @IBOutlet weak var changeLocalNicknameField: UITextField!
     
     @IBAction func submitPasswordClicked(_ sender: UIButton) {
-        let newPassword = changePasswordField.text
+        // let newPassword = changePasswordField.text
+        // TODO: Implement changePassword in DB, then finish this
         
     }
     
     @IBAction func submitGlobalNicknameClicked(_ sender: UIButton) {
         let newGlobalNickname = changeGlobalNicknameField.text
-        
+        database.setUserGlobalNickname(new_nickname: newGlobalNickname!)
     }
+    
     
     @IBAction func submitLocalNicknameClicked(_ sender: UIButton) {
         let newLocalNickname = changeLocalNicknameField.text
+        database.setUserLocalNickname(inHouseID: currentHouseID!, to: newLocalNickname!, view: self)
     }
     
     
