46c7d6614996353a636db6297da6a2a271a491ba
behrbaum <bbehrbaum22@gmail.com>
2018-03-21 18:53:31 -0400

    saves under specific users incomplete chores

--- Roommate App/Roommate App/Controllers/ViewController.swift
+++ Roommate App/Roommate App/Controllers/ViewController.swift
@@ -68,15 +68,17 @@ class ViewController: UIViewController {
     
     @IBAction func LogInButtonPressed(_ sender: Any) {
         database.login(username: usernameTextField.text!, password: passwordTextField.text!, view: self)
-	}    
-
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+}
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.destination is AllHousesPageViewController {
             let vc = segue.destination as? AllHousesPageViewController
             vc?.currentUser = userLoggingIn
             vc?.buttonToGetHere = buttonPressed
         }
     }
+    */
 
     @IBAction func CreateAccountButtonPressed(_ sender: Any) {
         database.createAccount(username: usernameTextField.text!, password: passwordTextField.text!, view: self)
