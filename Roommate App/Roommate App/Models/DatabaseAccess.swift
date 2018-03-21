6b7bf1e443391e5d3466fc4c0a0ed08e8fddc6df
Nick Buckenham <Nick Buckenham>
2018-03-21 18:41:43 -0400

    final changes, all working

--- Roommate App/Roommate App/Models/DatabaseAccess.swift
+++ Roommate App/Roommate App/Models/DatabaseAccess.swift
@@ -551,6 +551,16 @@ class DatabaseAccess  {
         return NoSuchHouseError()
     }
     
+    func changeHouseName(currHouseID : String, newName: String)-> ReturnValue<Bool> {
+        //Check if user is logged in
+        if Auth.auth().currentUser?.uid != nil {
+            // Setting value does not require closures and can be done directly to the DatabaseReferecece returned by .child() function
+            self.ref.child("houses/\(String(describing: currHouseID))/house_name").setValue(newName)
+            return ExpectedExecution()
+        }
+        return NoSuchHouseError()
+    }
+    
     //ELENA+Jesse - FIX COMMENTED FUNCTIONS
     
 //
