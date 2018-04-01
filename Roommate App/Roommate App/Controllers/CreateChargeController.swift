428132aeb1acc4ffb300757076656acf2e291c60
Jesse Berliner-Sachs <jessebs@me.com>
2018-04-01 19:55:25 -0400

    add charge page

--- Roommate App/Roommate App/Controllers/CreateChargeController.swift
+++ Roommate App/Roommate App/Controllers/CreateChargeController.swift
@@ -8,22 +8,24 @@
 
 import UIKit
 
-class CreateChargeController : UIViewController {
-
-    var cents : Int = 0
-    var charge : Charge?;
+class CreateChargeController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var charge : Charge?
+    var houseMemberUserNames : [String] = []
+    var selectedMembers : Set<Int> = Set()
     
     @IBOutlet weak var amountTextFeild: UITextField!
     @IBOutlet weak var chargePaySwitch: UISegmentedControl!
-    @IBOutlet weak var houseMemberPicker: UIPickerView!
     @IBOutlet weak var transactionDescriptionTextFeild: UITextView!
-    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         amountTextFeild.keyboardType = UIKeyboardType.numberPad
-        houseMemberPicker.reloadAllComponents()
-        
+        amountTextFeild.textAlignment = NSTextAlignment.right
         charge = nil
+        
+        houseMemberUserNames = ["Test House Member 1", "Test House Member 2", "Test House Member 3"]
+        selectedMembers.insert(0)
+        
     }
     
     private func getCentsFromTextField() -> Int {
@@ -35,13 +37,41 @@ class CreateChargeController : UIViewController {
     }
     
     private func getTextFromCents(cents: Int) -> String {
-       return "$ \(cents / 100).\(cents % 100)"
+        var decimal = String(cents % 100)
+        if cents % 100 < 10 {
+            decimal = "0\(decimal)"
+        }
+        return "$ \(cents / 100).\(decimal)"
     }
     
     @IBAction func DisplayText(_ sender: UITextField) {
         let newCents = getCentsFromTextField()
-        
-        
-        
+        amountTextFeild.text = getTextFromCents(cents: newCents);
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return houseMemberUserNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HouseMemberCell", for: indexPath)
+        cell.textLabel?.text = houseMemberUserNames[indexPath.row]
+        if selectedMembers.contains(indexPath.row) {
+            cell.backgroundColor = UIColor.green
+        } else {
+            cell.backgroundColor = UIColor.white
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if selectedMembers.contains(indexPath.row) {
+            selectedMembers.remove(indexPath.row)
+        } else {
+            selectedMembers.insert(indexPath.row)
+        }
+        print("caught pressed cell \(indexPath.row)")
+        tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.none)
+    }
+
 }
---
+++ Roommate App/Roommate App/Controllers/SelectHouseMembersTableController.swift
@@ -0,0 +1,13 @@
+//
+//  SelectHouseMembersTableController.swift
+//  Roommate App
+//
+//  Created by Jesse Berliner-Sachs on 4/1/18.
+//  Copyright © 2018 Team 20. All rights reserved.
+//
+
+import UIKit
+
+class SelectHouseMemebersTableController : UITableViewController {
+    
+}
