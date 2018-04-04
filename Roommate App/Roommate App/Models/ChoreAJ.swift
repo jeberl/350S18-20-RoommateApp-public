656141d2358f6d61ce0c3d513a14404e03b14e34
Ajani Motta <ajanimotta@vagelos-ve505-0554.apn.wlan.upenn.edu>
2018-04-04 19:46:55 -0400

    nudge implementation

--- Roommate App/Roommate App/Models/ChoreAJ.swift
+++ Roommate App/Roommate App/Models/ChoreAJ.swift
@@ -11,23 +11,29 @@ struct ChoreAJ {
     var choreID: String?
     var title: String
     var assignedBy: String
-    var assigned_to: String
+    var assignedTo: String
     var completed: Bool
     var timeAssigned: String
+    var lastTimeNudged: String?
+    var timesNudged : Int
     var timeCompleted: String?
     var houseID: String
     var description: String
+    
     
     init(choreTitle: String, assignor: String, assignee: String, houseID: String, description: String ){
         self.choreID = nil
         self.completed = false
         self.title = choreTitle
         self.assignedBy = assignor
-        self.assigned_to = assignee
+        self.assignedTo = assignee
         self.timeAssigned = DatabaseAccess.getInstance().getTimestampAsString()
+        self.lastTimeNudged = ""
+        self.timesNudged = 0
         self.timeCompleted = nil
         self.houseID = houseID
         self.description = description
+        
     }
     
     mutating func setChoreID(ID: String) {
