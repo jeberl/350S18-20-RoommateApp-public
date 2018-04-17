5d9a1c1016a23bb1e9792974990bade37cfb13e1
behrbaum <bbehrbaum22@gmail.com>
2018-04-16 20:23:53 -0400

    Flipping switch updates in da haus field in database

--- Roommate App/Roommate App/Controllers/SwitchTableViewCell.swift
+++ Roommate App/Roommate App/Controllers/SwitchTableViewCell.swift
@@ -6,24 +6,70 @@
 //  Copyright © 2018 Team 20. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 import FirebaseAuth
 
-class SwitchTableViewCell: UITableViewCell {
+class SwitchTableViewCell: UITableViewCell, CLLocationManagerDelegate {
 
+    @IBOutlet weak var inDaHausSwitch: UISwitch!
     let database : DatabaseAccess = DatabaseAccess.getInstance()
+    let locManager = CLLocationManager()
     
-    @IBAction func hausSwitch(_ sender: UISwitch) {
-        if (sender.isOn == true) {
+    @IBAction func switchClicked(_ sender: UISwitch) {
+        print("I CHANGED")
+        if sender.isOn {
+            print("In da haus switched on!")
             database.userInHouse(uid: (Auth.auth().currentUser?.uid)!, houseID: currentHouseID!)
         } else {
+            print("In da haus switched off!")
             database.userNotInHouse(uid: (Auth.auth().currentUser?.uid)!, houseID: currentHouseID!)
         }
     }
+    /*@IBAction func hausSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            let deviceLatitude = locManager.location?.coordinate.latitude
+            let deviceLongitude = locManager.location?.coordinate.longitude
+            let houseAddressClosure = { (address: String?) -> Void in
+                let streetAddress = address ?? ""
+                let geocoder = CLGeocoder()
+                geocoder.geocodeAddressString(streetAddress, completionHandler: {(placemarks, error) -> Void in
+                    if ((error) != nil){
+                        print("Error", error ?? "")
+                    }
+                    if let placemark = placemarks?.first {
+                        let coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+                        let lat = coordinates.latitude
+                        let lon = coordinates.longitude
+                        
+                    }
+                })
+            }
+            print("In da haus switched on!")
+            database.userInHouse(uid: (Auth.auth().currentUser?.uid)!, houseID: currentHouseID!)
+        } else {
+            print("In da haus switched off!")
+            database.userNotInHouse(uid: (Auth.auth().currentUser?.uid)!, houseID: currentHouseID!)
+        }
+    }*/
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        // Default switch to being what the user's in_da_haus status is in the database
+        let inDaHausClosure = { (isHome : Bool) -> Void in
+            if isHome {
+                print("In nib... isHome is true")
+                self.inDaHausSwitch.isOn = true
+            } else {
+                self.inDaHausSwitch.isOn = false
+                print("In nib... isHome is false")
+            }
+        }
+        print("IN NIB")
+        self.database.isUserHome(uid: (Auth.auth().currentUser?.uid)!, houseID: currentHouseID!,
+                                 callback: inDaHausClosure)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
